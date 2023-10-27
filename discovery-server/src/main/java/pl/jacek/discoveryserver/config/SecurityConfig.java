210979e054019b7318f7f2c30c82e3dd8fa01292
Jacek Wojcik <jacek.wojcik@example.com>
2023-10-27 16:46:53 +0200

    Adding security to api-gateway and discovery server

---
+++ discovery-server/src/main/java/pl/jacek/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package pl.jacek.discoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${EUREKA_USERNAME}")
+    private String username;
+    @Value("${EUREKA_PASSWORD}")
+    private String password;
+
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails userDetails = User
+                .withUsername(username)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance()::encode)
+                .password("{noop}"+password)
+                .authorities("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                        .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                        .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
